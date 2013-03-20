e2a2967dfc54da3c4d1b273eecb0f6e6deb81671
pezholio <pezholio@gmail.com>
2013-03-20 10:44:36 +0000

    Generate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfficeCalendar::Application.config.secret_token = 'fa432bdeeaa3c31aabda8b7ea295f94d6fd8f07727e3ca785f9e764405fb7b4d794a0a4f4953c940f29ce238dca851c38b15cdf7f2c44ff40113f2d9ddcfdac8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OfficeCalendar::Application.config.session_store :cookie_store, key: '_office-calendar_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OfficeCalendar::Application.config.session_store :active_record_store
