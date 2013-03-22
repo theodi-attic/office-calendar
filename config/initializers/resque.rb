06d7f821b9da005eb86efa8a7e5ad0bf847c61e4
pezholio <pezholio@gmail.com>
2013-03-22 09:32:35 +0000

    Setting up Resque properly

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,6 @@
 require 'resque'
 require 'resque_scheduler'
+require 'googlecalendar'
 
 Resque.redis = ENV['RESQUE_REDIS_HOST']
 Resque.schedule = YAML.load_file(Rails.root.join('config', 'schedule.yml'))
--- lib/tasks/resque.rake
+++ lib/tasks/resque.rake
@@ -1,3 +1,6 @@
-require "resque/tasks"
+require 'resque/tasks'
+require 'resque_scheduler/tasks'
 
-task "resque:setup" => :environment
+task "resque:setup" => :environment do
+  ENV['QUEUE'] = '*'
+end
