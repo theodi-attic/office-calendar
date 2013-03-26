ee1a87f1f40790ec19e649c2ee45c562db2fba6c
pezholio <pezholio@gmail.com>
2013-03-26 09:20:59 +0000

    Removed legacy methods

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,14 +2,6 @@ class Resource < ActiveRecord::Base
   
   attr_accessible :name, :email, :description, :type, :resourcetype, :active
   
-  def calendar_xml
-    "http://www.google.com/calendar/feeds/#{URI::escape(email)}/private/basic"
-  end
-  
-  def calendar_ical
-    "http://www.google.com/calendar/ical/#{URI::escape(email)}/private/basic.ics"
-  end
-  
   def events
     Rails.cache.fetch("gcal_events/#{email}", :expires_in => 1.hour) do
       GCalResources.get_events(email)
