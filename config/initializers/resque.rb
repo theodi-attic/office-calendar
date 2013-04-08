7aad64f577b30abfcf13e17f99da4dc2fa942412
pezholio <pezholio@gmail.com>
2013-04-08 11:28:49 +0100

    Need to connect to Redis using a password in production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,10 @@ require 'resque'
 require 'resque_scheduler'
 require 'import_resources'
 
-Resque.redis = ENV['RESQUE_REDIS_HOST']
+Resque.redis = Redis.new(
+  :host => ENV['RESQUE_REDIS_HOST'], 
+  :port =>  ENV['RESQUE_REDIS_PORT'],
+  :password => (ENV['RESQUE_REDIS_PASSWORD'].nil? || ENV['RESQUE_REDIS_PASSWORD']=='' ? nil : ENV['RESQUE_REDIS_PASSWORD'])
+)
+
 Resque.schedule = YAML.load_file(Rails.root.join('config', 'schedule.yml'))
