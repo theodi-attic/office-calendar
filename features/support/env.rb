fcccbc322f5a0bed44bdd648ae78fe4a963399c2
James Smith <james@floppy.org.uk>
2013-08-09 17:09:44 +0100

    Add simplecov support

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,11 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
+
 require 'cucumber/rails'
 require 'capybara/poltergeist'
 
